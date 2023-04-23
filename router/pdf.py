88b193342ccaf991ec3af1ae982767b92fc77bc9
unknown <as955046@gmail.com>
2023-04-23 12:09:23 +0800

    20230423_m4

--- router/pdf.py
+++ router/pdf.py
@@ -3,21 +3,11 @@ import random
 from datetime import timedelta,datetime
 import json
 from flask import Blueprint, views, render_template, request, jsonify, redirect, url_for, session
-import PyPDF2
+# import PyPDF2
 
 today=datetime.today().strftime("%Y-%m-%d")
 
 pdf_dp = Blueprint("pdf", __name__, url_prefix='/pdf')
-
-# PDFObj = open('ERP基礎檢定考試(學科)-簡約版.pdf', 'rb')
-# pdfReader = PyPDF2.PdfReader(PDFObj)
-# print(len(pdfReader.pages))
-#
-# PageObj = pdfReader.pages[0]
-# #可以取得內容文字(第幾頁)
-# ss=PageObj.extract_text()
-# print(ss)
-
 
 @pdf_dp.route("/getproduct")
 def getproduct():
--- router/pdfparse.py
+++ router/pdfparse.py
@@ -1,10 +1,37 @@
 #coding:UTF-8
 import PyPDF2
-PDFObj = open('ERP基礎檢定考試(學科)-簡約版.pdf', 'rb')
-pdfReader = PyPDF2.PdfReader(PDFObj)
-print(len(pdfReader.pages))
 
-PageObj = pdfReader.pages[0]
-#可以取得內容文字(第幾頁)
-ss=PageObj.extract_text()
-print(ss)
+
+class PdfParse():
+    def __init__(self):
+        self.pdfReader=None
+        self.pdfName=""
+        self.pdfCurrPage=0
+        self.pdfText=""
+
+    def open(self,filename):
+        self.pdfName=filename
+        PDFObj = open(self.pdfName, 'rb')
+        self.pdfReader =PyPDF2.PdfReader(PDFObj)
+
+    def close(self):
+        pass
+
+    def getText(self):
+        if not self.pdfReader:
+            return "Open PDF Reader"
+
+        for i in range(len(self.pdfReader.pages)):
+            PageObj = self.pdfReader.pages[0]
+            #可以取得內容文字(第幾頁)
+            ss=PageObj.extract_text()
+
+
+# PDFObj = open('ERP基礎檢定考試(學科)-簡約版.pdf', 'rb')
+# pdfReader = PyPDF2.PdfReader(PDFObj)
+# print(len(pdfReader.pages))
+#
+# PageObj = pdfReader.pages[0]
+# #可以取得內容文字(第幾頁)
+# ss=PageObj.extract_text()
+# print(ss)
