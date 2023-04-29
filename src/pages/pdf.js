b69f6145cda62256f2ef4f53d046c3cd4283d5df
unknown <as955046@gmail.com>
2023-04-29 10:37:00 +0800

    20230429_m11

--- src/pages/pdf.js
+++ src/pages/pdf.js
@@ -37,6 +37,10 @@ export default function Pdf() {
       // Update chosen files
       setFilesToUpload([ ...files ])
     };
+
+    const handleFileUploadError = (error) => {
+      // Do something...
+    }
   
     const uploadFiles = () => {
       // Create a form and post it to server
@@ -138,10 +142,43 @@ export default function Pdf() {
                     height: 240,
                   }}
                 >
-                 <FileUpload
-                  multiFile={false}
-                  onFilesChange={handleFilesChange}
-                  onContextReady={(context) => {}}
+                 <FileUpload 
+                   getBase64={false}
+                   multiFile={true}
+                   disabled={false}
+                   title="選擇檔案"
+                   header="[Drag to drop]"
+                   leftLabel="or"
+                   rightLabel="to select files"
+                   buttonLabel="click here"
+                   buttonRemoveLabel="Remove all"
+                   maxFileSize={10}
+                   maxUploadFiles={0}
+                   maxFilesContainerHeight={357}
+                   acceptedType={'image/*'}
+                   errorSizeMessage={'fill it or remove it to use the default error message'}
+                   allowedExtensions={['jpg', 'jpeg']}
+                   onFilesChange={handleFilesChange}
+                   onError={handleFileUploadError}
+                  //  imageSrc={'path/to/custom/image'}
+                   BannerProps={{ elevation: 0, variant: "outlined" }}
+                   showPlaceholderImage={true}
+                   PlaceholderGridProps={{ md: 4 }}
+                   LabelsGridProps={{ md: 8 }}
+                   onContextReady={context => {
+                     // access to component context here
+                   }}
+                   ContainerProps={{
+                     elevation: 0,
+                     variant: "outlined",
+                     sx: { p: 1 }
+                   }}
+                   PlaceholderImageDimension={{
+                     xs: { width: 128, height: 128 },
+                     sm: { width: 128, height: 128 },
+                     md: { width: 164, height: 164 },
+                     lg: { width: 256, height: 256 }
+                   }}
                  />
                  <Button variant="contained" onClick={uploadFiles}>上傳檔案</Button>      
                 </Paper>
