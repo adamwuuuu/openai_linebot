2a41cedc329e7ca0c8a7415ddcf36dbd7ad8de7e
unknown <as955046@gmail.com>
2023-04-29 20:55:34 +0800

    20230429_n4

--- chatgpt.py
+++ chatgpt.py
@@ -8,7 +8,8 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
-        self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003")
+        # self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003")
+        self.model = os.getenv("OPENAI_MODEL", default="text-davinci-003")
         #self.model = os.getenv("OPENAI_MODEL", default = "chatbot")
         self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
         self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
@@ -24,7 +25,7 @@ class ChatGPT:
             presence_penalty=self.presence_penalty,
             max_tokens=self.max_tokens
         )
-        print("OpenAI Response: %s"%(response))
+        # print("OpenAI Response: %s"%(response))
         return response['choices'][0]['text'].strip()
 
     def add_msg(self, text):
--- router/pdfparse.py
+++ router/pdfparse.py
@@ -56,7 +56,7 @@ class PdfParse():
                 self.questionText+=line
                 self.qlist.append(self.questionText.strip())
                 self.gptAnwserList.append(self.askGPT(self.questionText.strip()))
-             time.sleep(1)
+             time.sleep(2)
         # print("QNum: ",self.qnumlist)
         # print("Qlist: ",self.qlist)
         # print("Anwser: ", self.anwserlist)
@@ -67,4 +67,5 @@ class PdfParse():
         gpt.add_msg(f"Human:{text}?\n")
         reply_msg = gpt.get_response().replace("AI:", "", 1)
         gpt.add_msg(f"AI:{reply_msg}\n")
+        print("AI Respond: ",reply_msg)
         return reply_msg
